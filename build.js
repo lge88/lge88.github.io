968665038a2c2e43fd86bbbd7dfb7996a4ee484d
Li Ge <lge@ucsd.edu>
2013-06-06 17:32:26 -0700

    make gh-pages

---
+++ build.js
@@ -0,0 +1,24 @@
+require( 'shelljs/global' );
+
+var path = require('path');
+var jade = require('jade');
+
+// var pages = [ 'home', 'background', 'research', 'publications', 'misc' ];
+var pages = [ 'home', 'background' ];
+
+var src, dest, locals;
+
+cd( __dirname );
+
+pages.forEach( function( page ) {
+
+  src = '_jade/' + page + '.jade', dest = page + '.html';
+  locals = { 'navClass': {} };
+
+  locals.navClass[ page ] = 'active';
+
+  jade.compile( cat( src ), {
+    filename: src
+  })( locals ).to( dest );
+
+} );
